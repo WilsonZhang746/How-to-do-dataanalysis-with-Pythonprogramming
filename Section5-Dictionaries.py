1ce029c523596da5a45cd2f74fa30eae12ced8fd
WilsonZhang746 <46818760+WilsonZhang746@users.noreply.github.com>
2022-12-20 12:26:39 +0100

    Add files via upload

---
+++ Section5-Dictionaries.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 12:45:46 2022
+How to do data analysis with Python programming
+
+@author: https://www.youtube.com/@easydatascience2508
+"""
+
+##Section 5. Dictionaries
+
+#Lecture 1. Working with dictionaries
+#A simple example of dictionary
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+alien_0 = {'color': 'green', 'points': 5}
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+
+
+#Adding New Key-Value Pairs
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+#Starting with an Empty Dictionary
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(alien_0)
+
+
+#Modifying Values in a Dictionary
+alien_0 = {'color': 'green'}
+print("The alien is " + alien_0['color'] + ".")
+alien_0['color'] = 'yellow'
+print("The alien is now " + alien_0['color'] + ".")
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original position: " + str(alien_0['x_position']))
+
+# Move the alien to the right.
+# Figure out how far to move the alien based on its speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+
+# The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print("New position: " + str(alien_0['x_position']))
+
+#Removing Key-Value Pairs
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+del alien_0['points']
+print(alien_0)
+
+
+
+#Lecture 2. Looping Through a Dictionary
+#Looping Through All Key-Value Pairs
+user_0 = {'username': 'efermi',
+          'first': 'enrico',
+          'last': 'fermi',
+          }
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
+
+#A Dictionary of favorite programming languages for a number of people
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+        language.title() + ".")
+
+#Looping Through All the Keys in a Dictionary
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+for name in favorite_languages.keys():
+    print(name.title())
+
+
+#access the value associated with any key you care about inside the loop
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        print(" Hi " + name.title() +
+              ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
+        
+
+#use the keys() method to find out if a particular person was polled
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+    
+
+#Looping Through a Dictionary’s Keys in Order
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+
+#Looping Through All Values in a Dictionary
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+
+
+##Lecture 3. Nesting dictionaries
+#A List of Dictionaries
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+    
+# Make an empty list for storing aliens.
+aliens = []
+
+# Make 30 green aliens.
+for alien_number in range (0,30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+# Show the first 5 aliens:
+for alien in aliens[:5]:
+    print(alien)
+    print("...")
+
+# Show how many aliens have been created.
+print("Total number of aliens: " + str(len(aliens)))
+
+#to change the first three aliens to yellow, medium-speed aliens 
+#worth 10 points each
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+        
+
+#A List in a Dictionary
+# Store information about a pizza being ordered.
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+    }
+
+# Summarize the order.
+print("You ordered a " + pizza['crust'] + "-crust pizza " +
+      "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print("\t" + topping)
+    
+
+#A List in a Dictionary
+favorite_languages = {
+        'jen': ['python', 'ruby'],
+        'sarah': ['c'],
+        'edward': ['ruby', 'go'],
+        'phil': ['python', 'haskell'],
+        }
+for name, languages in favorite_languages.items():
+    print("\n" + name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())
+        
+        
+#A Dictionary in a Dictionary
+users = {'aeinstein': {'first': 'albert',
+                       'last': 'einstein',
+                       'location': 'princeton'},
+         'mcurie': {'first': 'marie',
+                    'last': 'curie',
+                    'location': 'paris'},
+         }
+
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
+        
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
