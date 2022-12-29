06fed9226d307ad03efed41766a1cb4db68e77d5
WilsonZhang746 <46818760+WilsonZhang746@users.noreply.github.com>
2022-12-29 14:26:01 +0100

    Add files via upload

--- Section7-Functions.py
+++ Section7-Functions.py
@@ -170,8 +170,174 @@ while True:
 
 
 
+##Lecture 6. Passing a List to function
+#Passing a List of names to a function called greet_users(),
+#which greets each person in the list individually
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
 
 
+
+#Modifying a List in a Function
+#does this without using functions
+# Start with some designs that need to be printed.
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+# Simulate printing each design, until none are left.
+# Move each design to completed_models after printing.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    # Simulate creating a 3D print from the design.
+    print("Printing model: " + current_design)
+    completed_models.append(current_design)
+
+# Display all completed models.
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
+    
+
+#Modifying a List in a Function, using 2 functions    
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until there are none left.
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+    
+        # Simulate creating a 3d print from the design.
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+        
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+        
+        
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+unprinted_designs  #empty now
+
+
+#If you do not want to modify the original list
+#You can use a copy of a list when you are calling the function
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs[:], completed_models)
+show_completed_models(completed_models)
+unprinted_designs      #not modified
+
+
+
+
+
+
+#Lecture 7. Passing an arbitrary number of arguments to function
+#Passing an Arbitrary Number of Arguments to Function
+def make_pizza(*toppings):
+    """Print the list of toppings that have been requested."""
+    print(toppings)
+    
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+#replace the print statement with a loop that runs through
+#the list of toppings and describes the pizza being ordered
+def make_pizza(*toppings):
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+        
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+
+
+#Mixing Positional and Arbitrary Arguments
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a " + str(size) +
+          "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+        
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+
+
+#Using Arbitrary Keyword Arguments
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='physics')
+print(user_profile)
+
+
+
+##Lecture 8. Storing Your Functions in Modules
+#Importing an Entire Module
+#A module file pizza.py, saved in the same directory as this 
+#source file in your computer.
+
+
+#Now we can import the module pizza we just created and 
+#then makes two calls to make_pizza():      
+import pizza
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+
+#Importing Specific Functions
+from pizza import make_pizza
+make_pizza(16, 'pepperoni')
+make_pizza(12, "mushrooms", "green peppers", "extra cheese")
+
+
+#Using as to Give a Function an Alias
+from pizza import make_pizza as mp
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+#Using as to Give a Module an Alias
+import pizza as p
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+#Importing All Functions in a Module
+#*** just know this form if you see another code 
+#best not use this form, however
+from pizza import *
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 
 
 
