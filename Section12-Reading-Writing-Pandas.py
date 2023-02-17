75741ecb6136d00e4a544471dfa25c15106cad93
WilsonZhang746 <46818760+WilsonZhang746@users.noreply.github.com>
2023-02-18 00:52:04 +0100

    Add files via upload

--- Section12-Reading-Writing-Pandas.py
+++ Section12-Reading-Writing-Pandas.py
@@ -382,30 +382,30 @@ etree2df(root)
 
 
 
-# ## JSON Data
+# ## Lecture 7.Reading and Writing JSON Data with Pandas
 
-# In[56]:
+import numpy as np
+import pandas as pd
 
+#setting working directory
+import os
+work_path="d:\\PythonBeginningCourse"
+os.chdir(work_path)      #setting new working directory
 
+#writing to a json file
 frame = pd.DataFrame(np.arange(16).reshape(4,4),
                     index=['white','black','red','blue'],
                     columns=['up','down','right','left'])
 frame.to_json('frame.json')
 
 
-# In[57]:
 
-
+##reading json file
 pd.read_json('frame.json')
-
-
-# In[58]:
 
 
 from pandas.io.json import json_normalize
 
-
-# In[59]:
 
 
 file = open('books.json','r')
@@ -413,311 +413,41 @@ text = file.read()
 text = pd.io.json.loads(text)
 
 
-# In[60]:
 
+pd.json_normalize(text,'books')
 
-json_normalize(text,'books')
 
 
-# In[61]:
+pd.json_normalize(text,'books',['writer','nationality'])
 
 
-json_normalize(text,'books',['writer','nationality'])
 
 
-# ## The Format HDF5
 
-# In[62]:
 
 
-from pandas.io.pytables import HDFStore
 
 
-# In[63]:
 
 
-frame = pd.DataFrame(np.arange(16).reshape(4,4),
-                    index=['white','black','red','blue'],
-                    columns=['up','down','right','left'])
-store = HDFStore('ch05_data.h5')
-store['obj1'] = frame
 
 
-# In[64]:
 
 
-frame2 = pd.DataFrame(np.arange(0,8,0.5).reshape(4,4))
-frame2
 
 
-# In[65]:
 
 
-store['obj2'] = frame2
 
 
-# In[66]:
 
 
-store
 
 
-# In[67]:
 
 
-store['obj2']
 
 
-# ## Pickle - Python Object Serialization
 
-# ### Serialize a Python Object with cPickle
 
-# In[68]:
-
-
-#On python3.x cPickle has changed from cPickle to _pickle. Thus in python3.x, you can do the following if you want to use cPickle:
-import _pickle as pickle
-
-
-# In[69]:
-
-
-data = { 'color': ['white','red'], 'value': [5, 7]}
-pickled_data = pickle.dumps(data)
-pickled_data
-
-
-# In[70]:
-
-
-print(pickled_data)
-
-
-# In[71]:
-
-
-nframe = pickle.loads(pickled_data)
-nframe
-
-
-# ### Pickling with pandas
-
-# In[72]:
-
-
-frame = pd.DataFrame(np.arange(16).reshape(4,4),
-                    index=['up','down','left','right'])
-frame.to_pickle('frame.pkl')
-
-
-# In[73]:
-
-
-pd.read_pickle('frame.pkl')
-
-
-# ## Interacting with Databases
-
-# In[74]:
-
-
-from sqlalchemy import create_engine
-
-
-# In[75]:
-
-
-engine = create_engine('postgresql://scott:tiger@localhost:5432/mydatabase')
-
-
-# ### Loading and Writing Data with SQLite3
-
-# In[76]:
-
-
-frame = pd.DataFrame(np.arange(20).reshape(4,5),
-                    columns=['white','red','blue','black','green'])
-frame
-
-
-# In[77]:
-
-
-engine = create_engine('sqlite:///foo.db')
-
-
-# In[78]:
-
-
-frame.to_sql('colors',engine)
-
-
-# In[79]:
-
-
-pd.read_sql('colors',engine)
-
-
-# In[80]:
-
-
-import sqlite3
-query = """
-         CREATE TABLE test
-         (a VARCHAR(20), b VARCHAR(20),
-          c REAL,        d INTEGER
-         );"""
-con = sqlite3.connect(':memory:')
-con.execute(query)
-
-
-# In[81]:
-
-
-con.commit()
-
-
-# In[82]:
-
-
-data = [('white','up',1,3),
-        ('black','down',2,8),
-        ('green','up',4,4),
-        ('red','down',5,5)]
-stmt = "INSERT INTO test VALUES(?,?,?,?)"
-con.executemany(stmt, data)
-
-
-# In[83]:
-
-
-con.commit()
-
-
-# In[84]:
-
-
-cursor = con.execute('select * from test')
-cursor
-
-
-# In[85]:
-
-
-rows = cursor.fetchall()
-rows
-
-
-# In[86]:
-
-
-cursor.description
-
-
-# In[87]:
-
-
-pd.DataFrame(rows, columns=zip(*cursor.description)[0])
-
-
-# ### Loading and Writing Data with PostgreSQL
-
-# In[88]:
-
-
-pd.__version__
-
-
-# In[90]:
-
-
-import psycopg2
-engine = create_engine('postgresql://postgres:password@localhost:5432/postgres')
-
-
-# In[92]:
-
-
-frame = pd.DataFrame(np.random.random((4,4)),
-                    index=['exp1','exp2','exp3','exp4'],
-                    columns=['feb','mar','apr','may']);
-frame.to_sql('dataframe',engine)
-
-
-# In[ ]:
-
-
-pd.read_sql_table('dataframe',engine)
-
-
-# In[ ]:
-
-
-pd.read_sql_query('SELECT index,apr,may FROM DATAFRAME WHERE apr > 0.5', engine)
-
-
-# ## Reading and Writing Data with a NoSQL Database: MongoDB
-
-# In[135]:
-
-
-import pymongo
-client = MongoClient('localhost',27017)
-
-
-# In[136]:
-
-
-db = client.mydatabase
-db
-
-
-# In[137]:
-
-
-client['mydatabase']
-
-
-# In[138]:
-
-
-collection = db.mycollection
-db['mycollection']
-
-
-# In[139]:
-
-
-collection
-
-
-# In[140]:
-
-
-frame = pd.DataFrame( np.arange(20).reshape(4,5), 
-                      columns = ['white','red','blue','black','green'])
-frame
-
-
-# In[141]:
-
-
-import json
-record = json.loads(frame.T.to_json()).values
-record
-
-
-# In[144]:
-
-
-collection.mydocument.insert(record)
-
-
-# In[145]:
-
-
-cursor = collection['mydocument'].find()
-dataframe = (list(cursor))
-del dataframe['_id']
-dataframe
 
