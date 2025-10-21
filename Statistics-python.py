c368ef74fae0ba7062f8c37a1a23c833b626e59d
WilsonZhang746 <46818760+WilsonZhang746@users.noreply.github.com>
2025-10-21 22:03:50 +0200

    Add files via upload

---
+++ Statistics-python.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 21 21:04:10 2025
+
+@author: Wilson
+"""
+
+### Geometric Mean
+import numpy as np
+import scipy.stats as stats
+
+x = np.arange(1,101)
+stats.gmean(x)
