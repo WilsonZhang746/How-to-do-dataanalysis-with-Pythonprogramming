9a5ce8f91a8adbace1c13b74e68b28b412b62c8a
WilsonZhang746 <46818760+WilsonZhang746@users.noreply.github.com>
2025-11-18 18:15:12 +0100

    Add files via upload

--- Section3-WorkingwithLists.py
+++ Section3-WorkingwithLists.py
@@ -490,6 +490,110 @@ for name, contents in drinks.items():
 
 
 
+### Continue Lines with backslash
+
+#not ussing backslash
+alphabet = ''
+alphabet += 'abcdefg'
+alphabet += 'hijklmnop'
+alphabet += 'qrstuv'
+alphabet += 'wxyz'
+
+
+#using backslash
+alphabet = 'abcdefg' + \
+    'hijklmnop' + \
+    'qrstuv' + \
+    'wxyz'
+
+
+
+
+
+
+
+
+
+### Examples of list comprehension
+
+#build a list of integers from 1 to 5, one item at a time
+number_list = []
+number_list.append(1)
+number_list.append(2)
+number_list.append(3)
+number_list.append(4)
+number_list.append(5)
+number_list
+
+#could also use an iterator and the range() function
+number_list = []
+for number in range(1, 6):
+    number_list.append(number)
+
+number_list
+
+
+#could just turn the output of range() into a list directly
+number_list = list(range(1, 6))
+number_list
+
+
+
+#Using list comprehension
+#[ expression for item in iterable ]
+
+number_list = [number for number in range(1,6)]
+number_list
+
+
+number_list = [number-1 for number in range(1,6)]
+number_list
+
+
+
+#A list comprehension can include a conditional expression
+
+#[ expression for item in iterable if condition ]
+
+#make a new comprehension that builds a list of only the 
+#odd numbers between 1 and 5 
+a_list = [number for number in range(1,6) if number % 2 == 1]
+a_list
+
+#more compact compared with its traditional counterpart
+a_list = []
+for number in range(1,6):
+    if number % 2 == 1:
+        a_list.append(number)
+
+a_list
+
+
+#can be more than one set of for clauses in comprehension.
+
+#using double for looping
+rows = range(1,4)
+cols = range(1,3)
+for row in rows:
+    for col in cols:
+        print(row, col)
+        
+#using list comprehension instead
+
+rows = range(1,4)
+cols = range(1,3)
+cells = [(row, col) for row in rows for col in cols]
+for cell in cells:
+    print(cell)
+
+
+#you can also use tuple unpacking the row and col values
+
+for row, col in cells:
+    print(row, col)
+
+
+
 
 
 
