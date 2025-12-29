c9f16ba294722c507bfd368e741598835d46b1f8
WilsonZhang746 <46818760+WilsonZhang746@users.noreply.github.com>
2025-12-29 21:52:18 +0100

    Add files via upload

--- Section10-The-NumPy-library.py
+++ Section10-The-NumPy-library.py
@@ -958,3 +958,72 @@ q
 
 
 
+
+
+
+
+
+
+
+
+
+### Understanding Numpy Reshape(-1, 1) and Reshape(1, -1)
+
+import numpy as np
+#reshape(-1, 1)
+
+#When you use reshape(-1, 1), you are asking numpy to reshape 
+#your array with 1 column and as many rows as necessary 
+#to accommodate the data. This operation will result in a 
+#2D array with a shape (n, 1), where n is the number of 
+#elements in your original array.
+
+# Original array
+arr = np.array([1, 2, 3, 4, 5, 6])
+print('Original array shape:', arr.shape)
+
+# Reshape array
+
+reshaped_arr = arr.reshape(2, 3)
+reshaped_arr
+print('Reshaped array shape:', reshaped_arr.shape)
+
+reshaped_arr = arr.reshape(-1, 1)
+print('Reshaped array shape:', reshaped_arr.shape)
+
+reshaped_arr
+
+
+
+# reshape(1, -1)
+
+#On the other hand, reshape(1, -1) reshapes your array 
+#with 1 row and as many columns as necessary to accommodate
+# the data. This operation will result in a 2D array with a 
+#shape (1, n), where n is the number of elements in your 
+#original array.
+
+
+import numpy as np
+
+# Original array
+arr = np.array([1, 2, 3, 4, 5, 6])
+print('Original array shape:', arr.shape)
+
+# Reshape array
+reshaped_arr = arr.reshape(1, -1)
+print('Reshaped array shape:', reshaped_arr.shape)
+
+reshaped_arr
+
+
+
+
+
+
+
+
+
+
+
+
